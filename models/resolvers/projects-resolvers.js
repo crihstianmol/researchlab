8c7b95209aff9c77a8f8a3b7414c940853b0a3c6
Crihstian Molina <crihstianmol@gmail.com>
2021-12-01 21:58:53 -0500

    Mutation added

--- models/resolvers/projects-resolvers.js
+++ models/resolvers/projects-resolvers.js
@@ -1,6 +1,24 @@
 import { ProjectModel } from '../schema/projects-schema.js';
 
 const projectResolvers = {
+    Mutation:{
+        createProject: async(root,args)=>{
+            let projectargs = args;
+            projectargs.phase = projectargs.phase.replace("_"," ");
+            console.log(projectargs)
+            const project = await ProjectModel.create({
+                projectName: projectargs.projectName,
+                budget: projectargs.budget * 1.001,
+                startDate: projectargs.startDate,
+                endDate: projectargs.endDate,
+                leaderId: projectargs.leaderId,
+                leaderName: projectargs.leaderName,
+                status: projectargs.status,
+                phase: projectargs.phase
+            });
+            return project;
+        }
+    },
     Query:{
         Project:async(root,args) =>{
             const project = await ProjectModel.findOne({_id:args._id})
