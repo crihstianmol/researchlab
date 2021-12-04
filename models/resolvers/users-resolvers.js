40252cbf0b03a001b063e74d6b3672d84f73b9c1
betancourtlsofie <sofia.betancourt@correounivalle.edu.co>
2021-12-04 14:04:07 -0500

    Añadir y editar usuario fueron añadidos

--- models/resolvers/users-resolvers.js
+++ models/resolvers/users-resolvers.js
@@ -1,20 +1,44 @@
-import { UserModel } from '../schema/users-schema.js';
+import { UserModel } from "../schema/users-schema.js";
 
 const userResolvers = {
-    Query:{
-        User: async(root,args)=>{
-            const user = await UserModel.findOne({_id : args._id})
-            return user
-        },
-        Users: async()=>{
-            const users = await UserModel.find()
-            return users
-        },
-        Students: async() =>{
-            const users = await UserModel.find({role: "Estudiante"})
-            return users
+  Mutation: {
+    createUser: async (root, args) => {
+      const userArgs = args;
+      userArgs.status = userArgs.status.replace("_", " ");
+      userArgs.role = userArgs.role.replace("_", " ");
+      const user = await UserModel.create(userArgs);
+      return user;
+    },
+    editUser: async (root, args) => {
+      const userArgs = args;
+      const user = await UserModel.findOneAndUpdate(
+        { _id: userArgs._id },
+        {
+          email: userArgs.email,
+          DNI: userArgs.DNI,
+          name: userArgs.name,
+          lastName: userArgs.lastName,
+          password: userArgs.password,
+          status: userArgs.status,
         }
-    }
+      );
+      return user;
+    },
+  },
+  Query: {
+    User: async (root, args) => {
+      const user = await UserModel.findOne({ _id: args._id });
+      return user;
+    },
+    Users: async () => {
+      const users = await UserModel.find();
+      return users;
+    },
+    Students: async () => {
+      const users = await UserModel.find({ role: "Estudiante" });
+      return users;
+    },
+  },
 };
 
-export {userResolvers};
+export { userResolvers };
