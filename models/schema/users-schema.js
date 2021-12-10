67c65f499cb2c52643702e3ae391c9913cbe767d
Crihstian Molina <crihstianmol@gmail.com>
2021-12-10 18:56:32 -0500

    fix

--- models/schema/users-schema.js
+++ models/schema/users-schema.js
@@ -32,7 +32,6 @@ const userSchema = new Schema(
         role: {
             type: String,
             enum: ['Estudiante', 'Lider', 'Admin'],
-            default:'Pendiente',
             required: true
         },
     },
